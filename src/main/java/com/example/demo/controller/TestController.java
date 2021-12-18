5c07cda462cda63bbd5188750121854af9615f01
zhongzengqiang <zhongzq@longjitech.com>
2021-12-19 02:13:25 +0800

    add cm,pv,pvc

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${app.env}")
+    private String envType;
+
     @GetMapping("test")
     public String test() {
         return "Hello World";
+    }
+
+    @GetMapping("config")
+    public String getConfig() {
+        return "ENV_TYPE:" + envType;
     }
 }
