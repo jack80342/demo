63ac28b0666c7be0c8ed8147f727fd4a43155ed0
zhongzengqiang <zhongzq@longjitech.com>
2021-12-20 14:47:31 +0800

    add default value

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,6 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 @RestController
 public class TestController {
 
@@ -18,5 +25,14 @@ public class TestController {
     @GetMapping("config")
     public String getConfig() {
         return "ENV_TYPE:" + envType;
+    }
+
+    @GetMapping("write")
+    public String write() throws IOException {
+        Path writePath = Paths.get("/home/hello.txt");
+        Files.deleteIfExists(writePath);
+        Files.createFile(writePath);
+        Files.write(writePath, "Hello World".getBytes(StandardCharsets.UTF_8), StandardOpenOption.TRUNCATE_EXISTING);
+        return "Done";
     }
 }
